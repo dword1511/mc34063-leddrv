6aada9ed56cabaa31f52f3681935c43b25a72730
dword1511 <zhangchi866@gmail.com>
2012-02-08 18:44:58 +0800

    Project initialization.

---
+++ 34063-led.cmd
@@ -0,0 +1,79 @@
+# Pin name action command file
+
+# Start of element D3
+ChangePinName(D3, 1, cathode)
+ChangePinName(D3, 2, anode)
+
+# Start of element J1
+ChangePinName(J1, 1, 1)
+ChangePinName(J1, 2, 2)
+
+# Start of element R5
+ChangePinName(R5, 1, 1)
+ChangePinName(R5, 2, 2)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element D2
+ChangePinName(D2, 2, K)
+ChangePinName(D2, 1, A)
+
+# Start of element C1
+ChangePinName(C1, 2, -)
+ChangePinName(C1, 1, +)
+
+# Start of element C2
+ChangePinName(C2, 2, 2)
+ChangePinName(C2, 1, 1)
+
+# Start of element D1
+ChangePinName(D1, 1, cathode)
+ChangePinName(D1, 2, anode)
+
+# Start of element C3
+ChangePinName(C3, 2, 2)
+ChangePinName(C3, 1, 1)
+
+# Start of element L1
+ChangePinName(L1, 2, 2)
+ChangePinName(L1, 1, 1)
+
+# Start of element R1C
+ChangePinName(R1C, 1, 1)
+ChangePinName(R1C, 2, 2)
+
+# Start of element R1B
+ChangePinName(R1B, 1, 1)
+ChangePinName(R1B, 2, 2)
+
+# Start of element R1A
+ChangePinName(R1A, 1, 1)
+ChangePinName(R1A, 2, 2)
+
+# Start of element C4
+ChangePinName(C4, 2, -)
+ChangePinName(C4, 1, +)
+
+# Start of element C5
+ChangePinName(C5, 2, 2)
+ChangePinName(C5, 1, 1)
+
+# Start of element R4
+ChangePinName(R4, 1, 1)
+ChangePinName(R4, 2, 2)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element U1
+ChangePinName(U1, 5, COMP)
+ChangePinName(U1, 6, VCC)
+ChangePinName(U1, 7, IPK)
+ChangePinName(U1, 8, COLL_D)
+ChangePinName(U1, 4, GND)
+ChangePinName(U1, 3, CT)
+ChangePinName(U1, 2, EMT_S)
+ChangePinName(U1, 1, COLL_S)
